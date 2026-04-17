28b91a146020a321ae371b4dcc72d2f72e260a67
Katchy <judahgaudy@gmail.com>
2026-04-17 10:17:16 -0700

    Finished movement

--- character.gd
+++ character.gd
@@ -1,28 +1,85 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
+# movement variables
+var speed
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 10.0
 const JUMP_VELOCITY = 4.5
+const SENSITIVITY = 0.003
 
+# head bobbing variables
+const BOB_FREQ = 2.8
+const BOB_AMP = 0.10
+var t_bob = 0.8
+
+# fov variables
+const BASE_FOV = 75.0
+const FOV_CHANGE = 5.0
+
+@onready var head = $Head
+@onready var camera = $Head/Camera3D
+
+# handle first person camera
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-50), deg_to_rad(50))
+		
+	if event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	if event is InputEventMouseButton and Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# add the gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# handle jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		
+	# handle sprint
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		speed = WALK_SPEED
+
+	# get the input direction and handle the movement/deceleration
+	var input_dir := Input.get_vector("left", "right", "up", "down")
+	var direction: Vector3 = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
+		
+	else:
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
+	
+	# head bobbing
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = _headbob(t_bob)
+	
+	# fov
+	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
+	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 0.8)
+	
 
 	move_and_slide()
+	
+func _headbob(time) -> Vector3:
+	var pose = Vector3.ZERO
+	pose.y = sin(t_bob * BOB_FREQ) * BOB_AMP
+	pose.x = cos(t_bob * BOB_FREQ / 2) * BOB_AMP
+	return pose
+	
